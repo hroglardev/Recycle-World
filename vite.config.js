c4aa000da95ca474f0997cd95f5162433684043d
Lucas Cubile <cubilelucas@gmail.com>
2024-06-09 18:36:11 -0300

    replace vite with webpack

--- vite.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  build: {
-    assetsInclude: ['**/*.png', '**/*.svg', 'fonts/*.ttf']
-  }
-};
---
+++ webpack.dev.cjs
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.cjs')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: 'dist'
+  },
+  optimization: {
+    usedExports: true
+  }
+})
